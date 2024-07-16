49220f096051df517fb68ab0854cdf624ec91794
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2024-07-16 23:48:48 +0530

    change swagger url

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello! from TODO rest API';
+    return `Hello! from TODO rest API <a href="/swagger">Click here to load Swagger Docs.</a>`;
   }
 }
